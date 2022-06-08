9ec7dc2572310226b054606b304e22c2a9344e7f
Sean Moriarty <spmoriarty1@gmail.com>
2022-06-08 15:27:41 -0700

    pages talking to eachother for episodes

--- src/EpisodeItem.js
+++ src/EpisodeItem.js
@@ -1,1 +1,12 @@
 
+
+export default function EpisodeItem({ title, plot, characters }) {
+  return (
+    <div> 
+      <h2>Episode: {title}</h2>
+      <p>What happens: {plot}</p>
+      <p>The players: {characters}</p>
+    </div>
+  );
+}
+
--- src/EpisodeList.js
+++ src/EpisodeList.js
@@ -1,7 +1,12 @@
+import EpisodeItem from './EpisodeItem.js';
 
-export default function EpisodeList() {
-  return (
-    <div>EpisodeList</div>
-  );
+
+export default function EpisodeList({ episodes }) {
+  
+  return (<div>
+    {
+      episodes.map((episode, i) => <EpisodeItem key={episode.title + i} {...episode.title.character} />)
+    }
+  </div>);
 }
 
